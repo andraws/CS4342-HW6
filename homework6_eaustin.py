43432206a74bfbd53a05924588b32210c89234a4
Emily Austin <bamboee@sbcglobal.net>
2021-05-06 14:23:09 -0400

    implemented train function

--- homework6_eaustin.py
+++ homework6_eaustin.py
@@ -28,37 +28,55 @@ def unpack(w):
 # return a vector w containing all of them.
 # This is useful for performing a gradient check with check_grad.
 def pack(W1, b1, W2, b2):
-    W = W1.flatten()
-    W = np.append(W, b1)
-    W = np.append(W, W2.flatten())
-    W = np.append(W, b2)
-    return W
+    return np.concatenate((W1.flatten(), b1.flatten(), W2.flatten(), b2.flatten()))
 
 
 # Load the images and labels from a specified dataset (train or test).
 def loadData(which):
-    images = np.load("fashion_mnist_{}_images.npy".format(which)) / 225
+    images = np.load("fashion_mnist_{}_images.npy".format(which))
     labels = np.load("fashion_mnist_{}_labels.npy".format(which))
-    labels = hoten(labels)
+    images = images / 255.00
+    labels = hotten(labels)
     return images, labels
+
+
+def reluprime(x):
+    return 1 * (x > 0)
+
+
+def relu(x):
+    return np.maximum(x, 0)
+
+
+def hotten(x):
+    return np.eye(NUM_OUTPUT)[x]
+
+
+# Each row returned sum equals 1, representing probability of its the guess
+# Returns a (N,10) matrix
+def softmax(x):
+    exp = np.exp(x)
+    sum_exp = np.sum(exp, axis=0)
+    return (exp / sum_exp[None, :]).T
+
+
+## Function that performs transformation and gives the softmax of that
+def get_yHat(X, w):
+    W1, b1, W2, b2 = unpack(w)
+    z1 = W1.dot(X.T) + b1[:, np.newaxis]  # (40,5)
+    h = relu(z1)  # (40,5)
+    z2 = W2.dot(h) + b2[:, np.newaxis]  # (10,5)
+    y_Hat = softmax(z2)  # (10.5)
+    return y_Hat;
 
 
 # Given training images X, associated labels Y, and a vector of combined weights
 # and bias terms w, compute and return the cross-entropy (CE) loss. You might
 # want to extend this function to return multiple arguments (in which case you
 # will also need to modify slightly the gradient check code below).
-def yhateq(x, w):
-    W1, b1, W2, b2 = unpack(w)
-    z1 = W1.dot(x.T) + b1[:, np.newaxis]  # (40,5)
-    h = relu(z1)  # (40,5)
-    z2 = W2.dot(h) + b2[:, np.newaxis]  # (10,5)
-    y_Hat = softmax(z2)  # (10.5)
-    return y_Hat
-
-
 def fCE(X, Y, w):
-    yhat = yhateq(X, w)
-    return np.sum((Y) * np.log(yhat)) * (-1 / Y.shape[0])
+    y_Hat = get_yHat(X, w)
+    return np.sum((Y) * np.log(y_Hat)) * (-1 / Y.shape[0])
 
 
 # Given training images X, associated labels Y, and a vector of combined weights
@@ -66,7 +84,8 @@ def fCE(X, Y, w):
 # want to extend this function to return multiple arguments (in which case you
 # will also need to modify slightly the gradient check code below).
 def gradCE(X, Y, w):
-    y_hat = yhateq(X, w)
+    W1, b1, W2, b2 = unpack(w)
+    y_hat = get_yHat(X, w);
 
     y_hatT = y_hat.T
     yT = Y.T
@@ -83,30 +102,29 @@ def gradCE(X, Y, w):
     grad_b1 = np.mean(g_T.T, axis=1)  # (40,1)
 
     gradPacked = pack(grad_W1, grad_b1, grad_W2, grad_b2)
+
     return gradPacked
 
 
 # Given training and testing datasets and an initial set of weights/biases b,
 # train the NN.
 def train(trainX, trainY, testX, testY, w):
-    pass
+    w = SGD(trainX, trainY, w, 0.01, 256)
+    print(fCE(testX, testY, w))
+    return w
 
 
-def relu(x):
-    return 1 * (x > 0)
-
-
-def reluprime(x):
-    return np.maximum(x, 0)
-
-
-def softmax(x):
-    exp = np.exp(x)
-    sum_exp = np.sum(exp, axis=0)
-    return (exp / sum_exp[None, :]).T
-
-def hoten(y):
-    return np.eye(NUM_OUTPUT)[y]
+def SGD(x, y, w, epsilon, bactchSize):
+    epoch = (x.shape[0] // bactchSize) - 1
+    epochnum = 1000
+    shuffle = np.random.permutation(y.shape[0])
+    x = x[shuffle, :]
+    y = y[shuffle, :]
+    for e in range(0, epochnum):
+        for i in range(0, epoch):
+            gradient = gradCE(x[0 + i * bactchSize:bactchSize + i * bactchSize, :], y[0 + i * bactchSize:bactchSize + i * bactchSize, :], w)
+            w = w - (epsilon * gradient)
+    return w
 
 
 if __name__ == "__main__":
@@ -120,17 +138,19 @@ if __name__ == "__main__":
     b1 = 0.01 * np.ones(NUM_HIDDEN)
     W2 = 2 * (np.random.random(size=(NUM_OUTPUT, NUM_HIDDEN)) / NUM_HIDDEN ** 0.5) - 1. / NUM_HIDDEN ** 0.5
     b2 = 0.01 * np.ones(NUM_OUTPUT)
-
     # Concatenate all the weights and biases into one vector; this is necessary for check_grad
     w = pack(W1, b1, W2, b2)
 
+    idxs = [1, 2, 3, 4, 5]
     # Check that the gradient is correct on just a few examples (randomly drawn).
     idxs = np.random.permutation(trainX.shape[0])[0:NUM_CHECK]
-    approx_fprime = scipy.optimize.approx_fprime(w, lambda W_: fCE(trainX[idxs, :], trainY[idxs, :], W_), 1e-8)
-    print(scipy.optimize.check_grad(lambda w_: fCE(np.atleast_2d(trainX[idxs, :]), np.atleast_2d(trainY[idxs, :]), w_), \
+    print("Check grad: ",
+          scipy.optimize.check_grad(lambda w_: fCE(np.atleast_2d(trainX[idxs, :]), np.atleast_2d(trainY[idxs, :]), w_), \
                                     lambda w_: gradCE(np.atleast_2d(trainX[idxs, :]), np.atleast_2d(trainY[idxs, :]),
                                                       w_), \
                                     w))
 
     # Train the network using SGD.
-    #train(trainX, trainY, testX, testY, w)
+    w = train(trainX, trainY, testX, testY, w)
+
+
